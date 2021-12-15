73c133ff4ae3253c404fd409297bf523791e0578
bsergiov <81264661+bsergiov@users.noreply.github.com>
2021-12-15 19:08:43 +0200

    Initial Commit

---
+++ localLogin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  localLogin
+//
+//  Created by Сергей Бровко on 15.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
